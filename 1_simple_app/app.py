37dafe8ab7c1d7fd6f3843cd906e33479f67d987
Vector Nguyen <vectornguyen76@gmail.com>
2022-09-23 08:52:10 +0700

    add docker-commands

--- 1_simple_app/app.py
+++ 1_simple_app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("index.html", message = "Hello World!")
+    return render_template("index.html", message = "Hello World 3!")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=3000)
